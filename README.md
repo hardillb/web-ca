1097be9d33b545f26dba67f9a581f1bd0909f2df
Ben Hardill <Benjamin.Hardill@dyson.com>
2020-02-18 10:51:04 +0000

    Added CA creator script
    Also added the right constraints to the personal/server certs.

--- README.md
+++ README.md
@@ -22,13 +22,7 @@ module.exports = {
 You can set the path to the CA's cert and key, set the default certificate life in days and 
 the port the service will listen on.
 
-## Usage
-
-Just point your browser at the correct host/port and then pick from either a host or personal
-certificate. Fill in the required fields including the passphrase for the CA key and a 
-passsphase for the output P12 file.
-
-## Running
+## Setup
 
 To allow running as a normal user and still binding to port 80 run the following.
 
@@ -36,9 +30,29 @@ To allow running as a normal user and still binding to port 80 run the following
 sudo setcap CAP_NET_BIND_SERVICE=+eip `which node`
 ```
 
+If you need to create a new CA cert then you can edit the `createCA.js` file to set the DN information for the CA
+and then run:
+
+```
+npm createCA -p <private key password for CA>
+```
+
+## Running
+
+```
+npm start
+```
+
+
+### Usage
+
+Just point your browser at the correct host/port and then pick from either a host or personal
+certificate. Fill in the required fields including the passphrase for the CA key and a 
+passsphase for the output P12 file.
+
 ## TODO
 
 - ~~Add a LOT of error handling~~
 - Allow the uploading of an old cert (to extract the Subject info) to create new
-- Add more constraints to cert types
+- ~~Add more constraints to cert types~~
 - ~~Add support to update CA serial/csr data~~
---
+++ createCA.js
@@ -0,0 +1,95 @@
+const fs = require('fs');
+const opensslCert = require('node-openssl-cert');
+const commandLineArgs = require('command-line-args');
+
+const optionsDefs = [
+	{ name: "caKeyPassword" , alias: "p", type: String },
+	{ name: "caPath", type: String, defaultOption: true}
+];
+
+const options = commandLineArgs(optionsDefs);
+
+var keyPassword = "verySecretPassword";
+var caPath = "./ca";
+
+if (!options.caKeyPassword) {
+	console.log("You need to supply a CA Key pssword ('-p <password>')")
+	process.exit(-1);
+}
+
+if (options.caPath) {
+	caPath = options.caPath;
+}
+
+const openssl = new opensslCert();
+
+const caKeyOptions = {
+	encryption: {
+		password: keyPassword,
+		cipher: "des3"
+	},
+	rsa_keygen_bits: 2048,
+	format: "PKCS8"
+};
+
+const csrOptions = {
+	hash: "sha256",
+	days: 3650,
+	subject: {
+		countryName: 'GB',
+		stateOrProvinceName: 'Gloucestershire',
+		localityName: 'Dursley',
+		organizationName: 'Hardill',
+		organizationalUnitName: 'CA',
+		commonName: 'Hardill Certificate Authority'
+	},
+	extensions: {
+		basicConstraints: {
+			critical: true,
+			CA: true,
+			pathlen: 0
+		},
+		keyUsage: {
+			critical: true,
+			usages: [
+				'digitalSignature',
+				'keyEncipherment',
+				'keyCertSign'
+			]
+		},
+		extendedKeyUsage: {
+			critical: true,
+			usages: [
+				'serverAuth',
+				'clientAuth'
+			]
+		}
+	}
+};
+
+if( !fs.existsSync('./ca')) {
+	fs.mkdirSync('./ca');
+	fs.mkdirSync('./ca/certs');
+	fs.writeFileSync("./ca/index.txt", "", {encoding: 'utf8'});
+
+	openssl.generateRSAPrivateKey(caKeyOptions, function(err, key, cmd){
+		if (err){
+			console.log(err);
+			return;
+		}
+		openssl.generateCSR(csrOptions, key, keyPassword, function(err, csr, cmd){
+			if (err){
+				console.log(err);
+				return;
+			}
+			openssl.selfSignCSR(csr, csrOptions, key, keyPassword, function(err, caCert, cmd){
+				if (err){
+					console.log(err);
+					return;
+				}	
+				fs.writeFileSync('./ca/ca.key', key, { encoding: 'utf8'});
+				fs.writeFileSync('./ca/ca.crt', caCert, { encoding: 'utf8'});
+			});
+		});
+	});
+};
